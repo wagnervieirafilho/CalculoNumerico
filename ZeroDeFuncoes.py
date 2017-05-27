5197e412c13c9067be0b8480a793d59224d0f9fe
wagnervieirafilho <wagnervieirafilho@gmail.com>
2017-05-27 15:28:32 -0300

    janle da bisseccao com alguns componentes

--- ZeroDeFuncoes.py
+++ ZeroDeFuncoes.py
@@ -5,6 +5,12 @@ import numpy as np
 import sys 								#não deixar de importar para usar graficos
 from PyQt4 import QtGui, QtCore			#não deixar de importar para usar graficos
 
+
+
+
+
+
+################################################################
 #classe da janela principal
 class Window(QtGui.QMainWindow):
 	#construtor
@@ -21,14 +27,17 @@ class Window(QtGui.QMainWindow):
 		#botao Bissecção
 		self.biBtn = QtGui.QPushButton("Bisseccao", self)
 		self.biBtn.move(200,50)
+		self.biBtn.clicked.connect(self.criaJanelaDoMetodoBis)
 
 		#botao Ponto fixo
 		self.pontoFixoBtn = QtGui.QPushButton("Ponto fixo", self)
 		self.pontoFixoBtn.move(200,100)
+		self.pontoFixoBtn.clicked.connect(self.criaJanelaDoMetodoPontFix)
 
 		#botao Newton
-		self.pontoFixoBtn = QtGui.QPushButton("Newton", self)
-		self.pontoFixoBtn.move(200,150)
+		self.newtonBtn = QtGui.QPushButton("Newton", self)
+		self.newtonBtn.move(200,150)
+		self.newtonBtn.clicked.connect(self.criaJanelaDoMetodoNewt)
 
 		#botao sair
 		self.quitBtn = QtGui.QPushButton("Sair", self)
@@ -36,6 +45,92 @@ class Window(QtGui.QMainWindow):
 		self.quitBtn.move(200,200)
 
 		self.show()
+
+	def criaJanelaDoMetodoBis(self):
+		self.janelaBisseccao = BisseccaoWindow()
+
+	def criaJanelaDoMetodoPontFix(self):
+		self.janelaPontoFixo = PontoWindow()
+
+	def criaJanelaDoMetodoNewt(self):
+		self.janelaNewton = NewtonWindow()
+######################################################################################
+
+
+
+
+
+
+#QMdiSubWindow
+#QMainWindow
+class BisseccaoWindow(QtGui.QMdiSubWindow):
+	#construtor
+	
+	def __init__(self):
+		super(BisseccaoWindow, self).__init__()
+		self.setGeometry(400,200,500,300)
+		self.setWindowTitle("Metodos da Bisseccao")
+		self.setWindowIcon(QtGui.QIcon('ico.png'))
+		
+		#define o layout da janela do método da bissecao
+		self.define_layout()
+
+	def define_layout(self):
+		#	etiqueta 'F(x)'
+		self.fDeX = QtGui.QLabel("f(x) = ",self)
+		self.fDeX.move(50,20)
+
+		#	campo para entrar com a função
+		self.funcao = QtGui.QLineEdit(self)
+		self.funcao.move(80,22)
+		self.funcao.resize(150,25)
+
+		#	etiqueta 'Erro'
+		self.erroLabel = QtGui.QLabel("Erro <= ",self)
+		self.erroLabel.move(250,20)
+		self.erroLabel.resize(150,25)
+
+		#	campo para entrar com o erro
+		self.campoErro = QtGui.QLineEdit(self)
+		self.campoErro.move(295,22)
+		self.campoErro.resize(50,25)
+
+		#	etiqueta 'iteracoes'
+		# self.iteracoesLabel = QtGui.QLabel("Numero de Iteracoes (k) = ",self)
+		# self.iteracoesLabel.move(50,70)
+		# self.iteracoesLabel.resize(150,25)
+
+		#	campo para entrar com o erro
+		# self.campoIteracoes = QtGui.QLineEdit(self)
+		# self.campoIteracoes.move(200,70)
+		# self.campoIteracoes.resize(50,25)
+		# self.campoIteracoes.setReadOnly(True)
+
+		#	etiqueta ' intervalo'
+		self.intervaloLabel = QtGui.QLabel("Intervalo: (                   ,                  )",self)
+		self.intervaloLabel.move(50,70)
+		self.intervaloLabel.resize(200,25)
+
+		#campos a e b do intervalo
+		self.campoA = QtGui.QLineEdit(self)
+		self.campoA.move(115,75)
+		self.campoA.resize(45,20)
+		self.campoB = QtGui.QLineEdit(self)
+		self.campoB.move(180,75)
+		self.campoB.resize(45,20)
+
+		self.show()
+
+
+
+
+
+
+
+
+
+
+
 
 def run():
 	app = QtGui.QApplication(sys.argv)
