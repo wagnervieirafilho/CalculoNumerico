bd1d8b88f94608aedf41627f700bf7247b408d02
wagnervieirafilho <wagnervieirafilho@gmail.com>
2017-05-27 00:27:34 -0300

    Primeiros botoes ainda sem funcao

--- ZeroDeFuncoes.py
+++ ZeroDeFuncoes.py
@@ -1,14 +1,45 @@
-#não deixar de importar para usar graficos
-import sys
-#não deixar de importar para usar graficos
-from PyQt4 import QtGui, QtCore
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import numpy as np
+import sys 								#não deixar de importar para usar graficos
+from PyQt4 import QtGui, QtCore			#não deixar de importar para usar graficos
 
-#basico pra criar janelas
-app = QtGui.QApplication(sys.argv)
-window = QtGui.QWidget()
-#setta tamanho e posicao
-window.setGeometry(50,50,500, 300)
-#titulo
-window.setWindowTitle("Primeira tela em python")
-window.show()
-########################
+#classe da janela principal
+class Window(QtGui.QMainWindow):
+	#construtor
+	def __init__(self):
+		super(Window, self).__init__()
+		self.setGeometry(400,200,500,300)
+		self.setWindowTitle("Metodos iterativos")
+		self.setWindowIcon(QtGui.QIcon('ico.png'))
+		
+		#define o layout da janela principal
+		self.home()
+
+	def home(self):
+		#botao Bissecção
+		self.biBtn = QtGui.QPushButton("Bisseccao", self)
+		self.biBtn.move(200,50)
+
+		#botao Ponto fixo
+		self.pontoFixoBtn = QtGui.QPushButton("Ponto fixo", self)
+		self.pontoFixoBtn.move(200,100)
+
+		#botao Newton
+		self.pontoFixoBtn = QtGui.QPushButton("Newton", self)
+		self.pontoFixoBtn.move(200,150)
+
+		#botao sair
+		self.quitBtn = QtGui.QPushButton("Sair", self)
+		self.quitBtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+		self.quitBtn.move(200,200)
+
+		self.show()
+
+def run():
+	app = QtGui.QApplication(sys.argv)
+	GUI = Window()
+	sys.exit(app.exec_())
+
+run()
